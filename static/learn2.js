d0d9bacec73fabd26d7443c34783294d784819c6
YifeiWen <96630740+YifeiWen@users.noreply.github.com>
2022-04-25 14:27:51 -0400

    Update learn2.js

--- static/learn2.js
+++ static/learn2.js
@@ -1,7 +1,7 @@
 
 function displayNames(titleData){
         console.log(titleData)
-        $(".basicTitle").append("<div>"+titleData[1].name+"</div>")
+        $(".basicTitle").append("<div>"+titleData[2].name+"</div>")
         $(".detailMan").append("<div>"+titleData[2].descriptionOne+"</div>")
         $(".detailZoom").append("<div>"+titleData[2].descriptionTwo+"</div>")
     }
